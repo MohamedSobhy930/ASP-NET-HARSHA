ff2679ce04367c76b3c3df609f314cde07fcedb1
mohamedsobhy <mohamedsobhi031@gmial.com>
2026-02-23 01:50:43 +0200

    Delete xUnit Project

--- xUnit/xUnit/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace xUnit.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-        public string? ErrorMessage { get; set; }
-    }
-}
